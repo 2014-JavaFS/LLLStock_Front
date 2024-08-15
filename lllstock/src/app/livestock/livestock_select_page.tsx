f46d5c45c7f6717b88c76ad3058f5bf3db560921
Tommy Hoang <tommy25hoang@gmail.com>
2024-08-15 14:44:48 -0500

    Deleted unneccessary SVGs and code connected to it Added base file structure for livestock data

---
+++ lllstock/src/app/livestock/livestock_select_page.tsx
@@ -0,0 +1,9 @@
+const Livestock_Select_Page = () => {
+     return (
+          //When an owner is selected, this page appears
+          //Dynamically render all cattle based on animal_id as clickable cards
+          <div></div>
+     );
+}
+
+export default Livestock_Select_Page;
---
+++ lllstock/src/app/livestock/livestock_table_page.tsx
@@ -0,0 +1,9 @@
+const Livestock_Select_Page = () => {
+     return (
+          //When a cattle is selected, render this page
+          //That cattle information is displayed. The option to edit will be on the top-right, the back button will be on the top-left
+          <div></div>
+     );
+}
+
+export default Livestock_Select_Page;
