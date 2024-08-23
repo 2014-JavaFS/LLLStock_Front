5c85501d6ff364503e0bdb2bbe06a23ca2a855a6
Tommy Hoang <tommy25hoang@gmail.com>
2024-08-23 15:24:38 -0500

    Added Generalized Error Page Home Button does not work yet

--- lllstock/src/app/page.tsx
+++ lllstock/src/app/page.tsx
@@ -2,13 +2,13 @@
 import Image from "next/image";
 import Link from "next/link";
 import Router, { useRouter } from "next/router";
-
+import Error from "./error/error_page";
 export default function Home() {
 
 
   return (
-    <main className="flex min-h-[calc(100vh-4em)] flex-col items-center justify-between p-24">
-      <p>This is the home page</p>
+    <main>
+      <Error />
     </main>
   );
 }
