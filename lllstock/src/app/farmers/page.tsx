d09135e86c18735162406901af5c36d0775a49da
Elrey Bernal <elreyjcbernal@gmail.com>
2024-08-23 11:09:26 -0500

    cleanup of code before pr part 2

--- lllstock/src/app/farmers/page.tsx
+++ lllstock/src/app/farmers/page.tsx
@@ -14,7 +14,7 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import FarmerCard from "./FarmerCard";
-import Farmer from "./[id]";
+
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
@@ -27,20 +27,6 @@ interface FarmerInformation {
   userType: string;
 }
 
-// const farmers: FarmerInformation[] = [
-//   { userId: 1, firstName: "John", lastName: "Smith", email: "js@mail.com"},
-//   { userId: 2, firstName: "Jane", lastName: "Doe", email: "jd@mail.com"},
-//   { userId: 3, firstName: "Emily", lastName: "Dickinson", email: "emilyd@mail.com"},
-//   { userId: 4, firstName: "John", lastName: "Smith", email: "js@mail.com"},
-//   { userId: 5, firstName: "Jane", lastName: "Doe", email: "jd@mail.com"},
-//   { userId: 6, firstName: "Emily", lastName: "Dickinson", email: "emilyd@mail.com"},
-//   { userId: 7, firstName: "John", lastName: "Smith", email: "js@mail.com"},
-//   { userId: 8, firstName: "Jane", lastName: "Doe", email: "jd@mail.com"},
-//   { userId: 9, firstName: "Emily", lastName: "Dickinson", email: "emilyd@mail.com"},
-//   { userId: 10, firstName: "John", lastName: "Smith", email: "js@mail.com"},
-//   { userId: 11, firstName: "Jane", lastName: "Doe", email: "jd@mail.com"},
-//   { userId: 12, firstName: "Emily", lastName: "Dickinson", email: "emilyd@mail.com"}
-// ]
 const Farmers: React.FC = () => {
 
   const [farmers, setFarmers] = useState<FarmerInformation[]>([]);
@@ -59,8 +45,6 @@ const Farmers: React.FC = () => {
   }, []);
 
   return (
-    // <main className="flex min-h-[calc(100vh-4em)] flex-col items-center justify-between p-24">
-    // </main>
     <>
     <p>This is the farmers page</p>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-4 p-4">
