2b63fb49e6933114ae503f68fda287203a94ad56
Tommy Hoang <55764170+Karituo-Luster@users.noreply.github.com>
2024-08-28 13:02:37 -0500

    Revert "Th edit user"

--- lllstock/src/utils/lllServer.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export const lllServer = axios.create({
-  baseURL: "http://localhost:8080",
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json",
-  },
-});
