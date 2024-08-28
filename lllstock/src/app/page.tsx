03b22bac538ea9967f949bd8b028da07476b8b41
Tommy Hoang <tommy25hoang@gmail.com>
2024-08-28 13:48:47 -0500

    Minor Login Page Fix

--- lllstock/src/app/page.tsx
+++ lllstock/src/app/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 import Image from "next/image";
-import LSP from "./livestock/page";
+import LSP from "./livestock_select/page";
 import LTP from "./livestock_table/page";
 import Link from "next/link";
 import Register from "./register/page";
 import Profile from "./user_profile/page";
-import Error from "./error/error_page";
+import Error from "./error/page";
 export default function Home() {
   return (
     <main className="flex-grow">
