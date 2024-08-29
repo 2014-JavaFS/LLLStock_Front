042c431a4cbb2af1b511e21af631397308a14224
Ryan Heron <rwh60@txstate.edu>
2024-08-29 13:05:16 -0500

    duck in home page

--- lllstock/src/app/Home/page.tsx
+++ lllstock/src/app/Home/page.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 const Home: React.FC = () => {
     return (
         <div>
+            <div className="w-1/2 bg-[url('https://cdn10.bostonmagazine.com/wp-content/uploads/sites/2/2021/08/rubber-duck-stock-t.jpg')] bg-cover bg-center"></div>
             <h1>Welcome to the Home Page</h1>
             <p>This is the content of the home page.</p>
         </div>
